2328649330ed204efaa1402b320f33b60665b99b
Jan Chaloupka <osobni@janchaloupka.cz>
2019-12-07 21:05:13 +0100

    Subscribe na lobby list observer

--- frontend/src/screens/LobbyList.tsx
+++ frontend/src/screens/LobbyList.tsx
@@ -2,8 +2,35 @@ import React from "react";
 import './LobbyList.scss';
 import LobbyListItem from "../components/LobbyListItem";
 import { withRouter, RouteComponentProps } from "react-router-dom";
+import { API } from "../logic/API";
+import { ServerEventType } from "../enums/ServerEventType";
+import { Lobby } from "../models/Lobby";
+import { ClientEventType } from "../enums/ClientEventType";
 
 class LobbyList extends React.Component<RouteComponentProps>{
+  ListItemChange(lobby: Lobby){
+    //console.log("Item change:", lobby);
+  }
+
+  ListItemNew(lobby: Lobby){
+    //console.log("New item:", lobby);
+  }
+
+  ListItemRemove(lobby: Lobby){
+    //console.log("Item removed:", lobby);
+  }
+
+  componentDidMount(){
+    API.Subscribe(ServerEventType.LobbyListItemChange, (data) => this.ListItemChange(data));
+    API.Subscribe(ServerEventType.LobbyListItemNew, (data) => this.ListItemNew(data));
+    API.Subscribe(ServerEventType.LobbyListItemRemove, (data) => this.ListItemRemove(data));
+    API.SendEvent({Type: ClientEventType.SubscribeLobbyList});
+  }
+
+  componentWillUnmount(){
+    API.SendEvent({Type: ClientEventType.UnsubscribeLobbyList});
+  }
+
   render(){
     return (
       <div className="LobbyList">
