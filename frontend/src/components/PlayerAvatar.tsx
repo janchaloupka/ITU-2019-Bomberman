1aeab14ec76bdd90af4683616ca88c2b47b98bb2
Jan Chaloupka <osobni@janchaloupka.cz>
2019-12-09 10:56:38 +0100

    Verze na obhajobu

--- frontend/src/components/PlayerAvatar.tsx
+++ frontend/src/components/PlayerAvatar.tsx
@@ -8,14 +8,14 @@ import "../assets/avatar_destroyer.png";
 interface PlayerAvatarProps{
   name: string;
   color: number;
-  isHost?: boolean;
+  small?: boolean;
   character: string;
 }
 
 class PlayerAvatar extends React.Component<PlayerAvatarProps>{
   render(){
     return (
-      <div className="PlayerAvatar">
+      <div className={"PlayerAvatar " + (this.props.small ? "Small" : "")}>
         <div style={{filter: `${(this.props.color > 50 && this.props.color < 250) ? 'brightness(150%) ' : ''}hue-rotate(${this.props.color}deg)`}} className={"Avatar " + this.props.character}></div>
         <div className="Name">{this.props.name}</div>
       </div>
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import './Game.scss';
+import '../assets/Overworld.png';
+import '../assets/IceMap.png';
+import '../assets/FootballPitch.png';
+import '../assets/player_regular.png';
 import PlayerAvatar from "../components/PlayerAvatar";
 import HealthBar from "../components/HealthBar";
-import Countdown from 'react-countdown-now';
+import Countdown, { zeroPad } from 'react-countdown-now';
 import Bomb from "../components/Bomb";
 import { GameManager } from "../logic/GameManager";
-import { RouteComponentProps, withRouter } from "react-router-dom";
+import { RouteComponentProps, withRouter, Router } from "react-router-dom";
 import { Lobby as LobbyModel } from "../models/Lobby";
+import { API } from "../logic/API";
+import { ClientEventType } from "../enums/ClientEventType";
 
 interface GameState extends LobbyModel{
-
 }
 
 class Game extends React.Component<RouteComponentProps, GameState>{
@@ -23,18 +28,36 @@ class Game extends React.Component<RouteComponentProps, GameState>{
         Map: {ID: "", Objects: [], Name: ""}
     }
 
+    private subscribedUpdate = (() => {});
+
     componentDidMount(){
+      if(!GameManager.CurrentLobby){
+        this.props.history.replace("/");
+      }
+      this.subscribedUpdate = () => this.GameUpdate();
+      GameManager.SubscribeLobbyChange(this.subscribedUpdate);
+      this.GameUpdate();
+    }
+
+    componentWillUnmount(){
+      GameManager.UnsubscribeLobbyChange(this.subscribedUpdate);
+    }
+
+    GameUpdate(){
       if(!GameManager.CurrentLobby) return;
+      if(GameManager.CurrentLobby.Players.length === 1) API.SendEvent({Type: ClientEventType.LeaveLobby});
       this.setState(GameManager.CurrentLobby);
     }
 
     renderOpponents(){
         let players = this.state.Players.filter((p) => p.ID !== this.state.YourID);
-        if(players.length === 0) return <p>Čeká se na hráče...<br/>Adresa pro připojení: <b>{window.location.host}{window.location.pathname}</b></p>;
+        if(players.length === 0){
+          return [];
+        }
 
         return players.map((p) => {
           let i = this.state.Players.indexOf(p);
-          return (<section className="OtherPlayers"><PlayerAvatar key={p.ID} name={p.Nick} character={p.Character.ID} color={100*i} /> <HealthBar heart1="red_heart" heart2="red_heart" heart3="gray_heart" /></section>) 
+          return (<section className="OtherPlayers"><PlayerAvatar small={true} key={p.ID} name={p.Nick} character={p.Character.ID} color={100*i} /> <HealthBar heart1="red_heart" heart2="red_heart" heart3="gray_heart" /></section>)
         });
     }
 
@@ -45,7 +68,7 @@ class Game extends React.Component<RouteComponentProps, GameState>{
 
         return(
             <section className="CurrentPlayer">
-                <PlayerAvatar key={player.ID} name={player.Nick} character={player.Character.ID} color={100*i}/>
+                <PlayerAvatar small={true} key={player.ID} name={player.Nick} character={player.Character.ID} color={100*i}/>
                 <section className="Attributes">
                     <HealthBar heart1="red_heart" heart2="red_heart" heart3="gray_heart" />
                     <Bomb bombsLeft={3}/>
@@ -54,25 +77,27 @@ class Game extends React.Component<RouteComponentProps, GameState>{
         );
     }
 
+    // @ts-ignore
+    countdownRenderer({ minutes, seconds }) {
+      return <span> {zeroPad(minutes)}:{zeroPad(seconds)}</span>;
+    };
+
     render(){
-        return(
-        <div className = 'Game'>
-            <div className = 'ScreenContent' />
-
-            <div className = 'Bar'>
-
+      return(
+      <div className = 'Game'>
+        <div className = 'ScreenContent'>
+          <div className = {'Map ' + this.state.Map.ID} />
+          <div className = 'Bar'>
             { this.renderSelf() }
-
-                <section className="Countdown">
-                    <Countdown date={Date.now() + this.state.TimeLimit * 1000}/>
-                </section>
-
-
-                { this.renderOpponents() }
-
-            </div>
+            <section className="Countdown">
+              <Countdown renderer={this.countdownRenderer} date={Date.now() + this.state.TimeLimit * 1000}/>
+            </section>
+            { this.renderOpponents() }
+          </div>
         </div>
-        );}
+      </div>
+      );
+    }
 }
 
 export default withRouter(Game);
